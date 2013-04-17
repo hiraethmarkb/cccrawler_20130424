62633e4c2318dddd00dfef3746ae60b7dffc2eba
Mark Burns <thedarkmane@googlemail.com>
2013-04-17 21:32:33 +0100

    Add code to handle movement
    modified:   cccrawler.py

--- cccrawler/cccrawler.py
+++ cccrawler/cccrawler.py
@@ -6,14 +6,45 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
+# check for pressed keys, and update player position
+def handle_keys():
+  global playerx, playery
+
+  key = libtcod.console_check_for_keypress()
+  if key.vk == libtcod.KEY_ENTER and key.lalt:
+    # Alt+Enter: toggle fullscreen
+    libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+  elif key.vk == libtcod.KEY_ESCAPE
+    return True # exit game
+
+  # movement keys
+  if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+    playery -= 1
+
+  elif libtcod.console_key_is_pressed(libtcod.KEY_DOWN):
+    playery += 1
+
+  elif libtcod.console_key_is_pressed(libtcod.KEY_LEFT):
+    playerx -= 1
+
+  elif libtcod.console_key_is_pressed(libtcod.KEY_RIGHT):
+    playerx += 1
+
+###########################################################
+# Initialization & Main Loop
+###########################################################
+
 # set font
-libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
-
+libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYP    E_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 # initialize window
-libt.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'CC Crawler', False)
-
+libt.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'CC Crawler',     False)
 # we're going real-time, so limit game speed
 libtcod.sys_set_fps(LIMIT_FPS)
+ 
+# create variables for tracking the players position
+playerx = SCREEN_WIDTH/2
+playery = SCREEN_WIDTH/2
 
 # main game loop, runs until the window is closed
 while not libtcod.console_is_window_closed():
@@ -23,3 +54,8 @@ while not libtcod.console_is_window_closed():
   libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
   # flush console, to output to screen
   libtcod.console_flush()
+
+  # handle keys and exit game if needed
+  exit = handle_keys()
+  if exit:
+    break
