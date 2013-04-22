d326d3578e3bcead21a81f1fef2524b923f88c64
Mark Burns <thedarkmane@googlemail.com>
2013-04-22 21:25:18 +0100

    Objectify
    Add class definition for movable objects, players, npcs
    
    modified:   cccrawler.py

--- cccrawler.py
+++ cccrawler.py
@@ -8,6 +8,29 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
+class Object:
+  #this is a generic object: the player, a monster, an item, the stairs...
+  #it's always represented by a character on screen.
+  def __init__(self, x, y, char, color):
+    self.x = x
+    self.y = y
+    self.char = char
+    self.color = color
+ 
+  def move(self, dx, dy):
+    #move by the given amount
+    self.x += dx
+    self.y += dy
+ 
+  def draw(self):
+    #set the color and then draw the character that represents this object at its position
+    libtcod.console_set_default_foreground(con, self.color)
+    libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+ 
+  def clear(self):
+    #erase the character that represents this object
+    libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
 # check for pressed keys, and update player position
 def handle_keys():
   global playerx, playery
@@ -23,16 +46,16 @@ def handle_keys():
 
   # movement keys
   if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-    playery -= 1
+    player.move(0, -1)
 
   elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-    playery += 1
+    player.move(0, 1)
 
   elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-    playerx -= 1
+    player.move(-1, 0)
 
   elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-    playerx += 1
+    player.move(1,0)
 
 ###########################################################
 # Initialization & Main Loop
@@ -46,26 +69,32 @@ libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'CodeCumbria Crawler', Fa
 # create new off-screen console, for drawing stuff
 con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 # we're going real-time, so limit game speed
-#libtcod.sys_set_fps(LIMIT_FPS)
+libtcod.sys_set_fps(LIMIT_FPS)
  
-# create variables for tracking the players position
-playerx = SCREEN_WIDTH/2
-playery = SCREEN_WIDTH/2
+#create object representing the player
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+ 
+#create an NPC
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+ 
+#the list of objects with those two
+objects = [npc, player]
 
 # main game loop, runs until the window is closed
 while not libtcod.console_is_window_closed():
-  # set text colour to white
-  libtcod.console_set_default_foreground(con, libtcod.white)
-  # character starting position
-  libtcod.console_put_char(con, 1, 1, '@', libtcod.BKGND_NONE)
+  
+  #draw all objects in the list
+  for object in objects:
+    object.draw()
 
+  #blit the contents of "con" to the root console and present it
   libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
-
-  # flush console, to output to screen
   libtcod.console_flush()
-
-  libtcod.console_put_char(con, playerx, playery, ' ', libtcod.BKGND_NONE)
-
+ 
+  #erase all objects at their old locations, before they move
+  for object in objects:
+    object.clear()
+  
   # handle keys and exit game if needed
   exit = handle_keys()
   if exit:
